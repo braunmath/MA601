0baf3a59551b2d7ec35c8f387df9946aaa0a7b6a
Benjamin Braun <38571546+braunmath@users.noreply.github.com>
2026-08-18 20:26:28 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -25,7 +25,79 @@ var ptx_lunr_docs = [
   "type": "Chapter",
   "number": "2",
   "title": "Responding to Student Questions",
-  "body": " Responding to Student Questions  TO COMPLETE.  "
+  "body": " Responding to Student Questions   Pre-class activity    Read the MA 601 syllabus.    Think about a few of your previous professors who you felt had excellent skills at responding to student questions. Make a written list of 2-3 things that they did when responding to questions that you believe made their responses effective? Be prepared to share your list with the class.     We will begin class with the following activity.   Discuss with your neighbors: what is one thing that happened during your first two days of recitations that went well? What is one thing that happened that did not go well?   In these notes, I am going to use the phrase novice teacher to refer to anyone who is within their first five years of teaching experience, either in K-12 or post-secondary institutions. It is possible that some of you have more experience than this, but most of you fall within the novice category. I want to emphasize that novice does not equal bad! Novice simply refers to the amount of experience you have in teaching. A novice teacher is someone who has a limited amount of experience, and thus has not had enough teaching time yet to develop and incorporate a really broad range of teaching skills, styles, and methods.   Share with your neighbors some of the good qualities you observed from your previous professors in responding to student questions.    Discuss with your neighbors: what are 2-3 qualities that you all agree are part of an \"excellent response\" to a student question?   One of the most important actions you will take as a recitation leader, and as a teacher more generally, is to respond to student questions. Many novice teachers hear the phrase   \"respond to student questions\"   and interpret it as   \"explain the answer to student questions\".   The main goal for today is to develop a deeper understanding of more effective ways to interpret the phrase \"respond to student questions\".  Student questions fall into many different types, here are four examples:    Typos or simple errors : questions of this type do not reflect any misunderstanding on the part of the student, just a very simple error.     Moments of confusion : these are questions where students have correctly started trying to solve a problem, but they get stuck and are confused by their own work and\/or by what their goal is for solving the problem.     Lack of an \"entry point\" : these questions arise when students do not have any idea how to start working on a problem, they cannot identify an \"entry point\" to begin a solution.     Incorrect techniques : students might have questions because they are attempting to use a technique incorrectly, and are getting errors as a result.   When a student asks you a question, your first goal is to \"diagnose\" what has caused the student to ask a question . What is hard about this is that the student might not know what the cause of their question really is .  Many of you might have heard of the teaching technique of \"Socratic dialogue\", where teachers respond to student questions by asking follow-up questions, leading students to answer their own questions. For novice teachers in math, your goal should be to develop solid techniques and skills that are halfway between pure \"Socratic dialogue\" and pure \"tell the student the answer\". Your goal is to learn to respond to a student question with a series of questions that diagnose what is really causing their question, and then to be able to guide the student to the correct answer through a series of clear explanations and hints.   This is very hard to do consistently, especially in the moment right after a question is asked. So, let's think of your goal as novice teachers to develop the following method of responding to student questions, and to implement it consistently.   Step 1: Ask the student follow-up questions. Sometimes these questions are not related to their original question!    Step 2: Make an educated guess about what \"really\" caused the student to ask their question. Often, the student is not aware of what is causing their confusion or error or misunderstanding etc.!    Step 3: Engage the student with a clear hint or a clear explanation that will direct their attention toward the cause of their question.      Discuss with your neighbors: Suppose Student A is supposed to expand the product and their answer is not agreeing with what Student B found. What process might you go through to respond to this question?    Discuss with your neighbors: Suppose a student is trying to take the derivative of and they tell you \"I don't even know what I'm supposed to do, can you help me?\". What process might you go through to respond to this question?    Sometimes, it is appropriate to respond to a student question by simply \"telling them the answer\". What are examples of such situations?    Discuss with your neighbors: What are some questions that students have asked you during your first two recitations? How did you respond to them? Was the response effective? Would you respond differently if you could do it again?    General class discussion: do you understand this idea of \"diagnosing\" the cause of the question prior to responding? What questions do you have about responding to questions?   "
+},
+{
+  "id": "ch-responding-4",
+  "level": "2",
+  "url": "ch-responding.html#ch-responding-4",
+  "type": "Activity",
+  "number": "2.0.1",
+  "title": "",
+  "body": " Discuss with your neighbors: what is one thing that happened during your first two days of recitations that went well? What is one thing that happened that did not go well?  "
+},
+{
+  "id": "ch-responding-6",
+  "level": "2",
+  "url": "ch-responding.html#ch-responding-6",
+  "type": "Activity",
+  "number": "2.0.2",
+  "title": "",
+  "body": " Share with your neighbors some of the good qualities you observed from your previous professors in responding to student questions.  "
+},
+{
+  "id": "ch-responding-7",
+  "level": "2",
+  "url": "ch-responding.html#ch-responding-7",
+  "type": "Activity",
+  "number": "2.0.3",
+  "title": "",
+  "body": " Discuss with your neighbors: what are 2-3 qualities that you all agree are part of an \"excellent response\" to a student question?  "
+},
+{
+  "id": "ch-responding-16",
+  "level": "2",
+  "url": "ch-responding.html#ch-responding-16",
+  "type": "Activity",
+  "number": "2.0.4",
+  "title": "",
+  "body": " Discuss with your neighbors: Suppose Student A is supposed to expand the product and their answer is not agreeing with what Student B found. What process might you go through to respond to this question?  "
+},
+{
+  "id": "ch-responding-17",
+  "level": "2",
+  "url": "ch-responding.html#ch-responding-17",
+  "type": "Activity",
+  "number": "2.0.5",
+  "title": "",
+  "body": " Discuss with your neighbors: Suppose a student is trying to take the derivative of and they tell you \"I don't even know what I'm supposed to do, can you help me?\". What process might you go through to respond to this question?  "
+},
+{
+  "id": "ch-responding-18",
+  "level": "2",
+  "url": "ch-responding.html#ch-responding-18",
+  "type": "Activity",
+  "number": "2.0.6",
+  "title": "",
+  "body": " Sometimes, it is appropriate to respond to a student question by simply \"telling them the answer\". What are examples of such situations?  "
+},
+{
+  "id": "ch-responding-19",
+  "level": "2",
+  "url": "ch-responding.html#ch-responding-19",
+  "type": "Activity",
+  "number": "2.0.7",
+  "title": "",
+  "body": " Discuss with your neighbors: What are some questions that students have asked you during your first two recitations? How did you respond to them? Was the response effective? Would you respond differently if you could do it again?  "
+},
+{
+  "id": "ch-responding-20",
+  "level": "2",
+  "url": "ch-responding.html#ch-responding-20",
+  "type": "Activity",
+  "number": "2.0.8",
+  "title": "",
+  "body": " General class discussion: do you understand this idea of \"diagnosing\" the cause of the question prior to responding? What questions do you have about responding to questions?  "
 },
 {
   "id": "ch-grading",
